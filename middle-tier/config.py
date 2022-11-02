3864bf68862da127dadde361f4e592e3b270b177
huttsMichael <huttsmichael@gmail.com>
2022-11-02 19:07:37 -0400

    better documented config file checking

--- middle-tier/config.py
+++ middle-tier/config.py
@@ -5,24 +5,27 @@ from tkinter.filedialog import askopenfilename
 
 def check() -> None:
     config = configparser.ConfigParser()
-
     logger = logging.getLogger(__name__)    
 
+    # check if config file already exists
     try:
         logger.info("Loading config")
         with open("config/middle-tier.ini") as config_file:
             config.read_file(config_file)
         logger.debug(f"bizhawk exe path: {config['directories']['bizhawk']}")
-
+    # create config file if it doesn't
     except:
         logging.warning("No config found, setting up...")
         print("Where is the bizhawk excecutable?")
-        dir_bizhawk = askopenfilename()
+        # bizhawk excecutable is currently unused
+        # currently just saving for the sake of an example
+        dir_bizhawk = askopenfilename() # prompt user to select excecutable
 
         config['directories'] = {
             "bizhawk": dir_bizhawk
         }
 
+        # creat file
         with open('config/middle-tier.ini', 'w') as configfile:
             config.write(configfile)
     
