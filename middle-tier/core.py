6043a392e6100d5fd7de48ecb7d1d33a1a25e981
huttsMichael <huttsmichael@gmail.com>
2022-11-03 16:16:58 -0400

    updated core functions

--- middle-tier/core.py
+++ middle-tier/core.py
@@ -1,8 +1,10 @@
 import games
 
-def demo():
+# TODO: move demos to their own testing file
+def demo_sample():
     sample = games.load("game-data/sample.json")
     sample.info()
 
-def run():
-    pass
+def run(game_path: str):
+    game = games.load(game_path)
+    game.info()
